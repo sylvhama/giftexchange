e3d6169ccf4a04108a8b72e6f86b8df78976842a
Sylvain Hamann <sylvhama@gmail.com>
2017-04-05 17:54:43 +0900

    Prepared app

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015-native-modules']
+        }
+      }
+    ],
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    historyApiFallback: true,
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/$/, to: './index.html' },
+        { from: /\/admin/, to: './admin.html' },
+        { from: /./, to: './404.html' }
+      ]
+    }
+  }
+};
