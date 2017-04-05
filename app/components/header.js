6fde58accacd2c5beeb8e2cc638f954f6d6213bd
Sylvain Hamann <sylvhama@gmail.com>
2017-04-05 19:28:38 +0900

    small style update and first component added

---
+++ app/components/header.js
@@ -0,0 +1,9 @@
+export default function header(title) {
+  return `
+    <header class="header">
+      <h1 class="header__title">
+        ${title}
+      </h1>
+    </header>
+  `;
+}
--- app/index.js
+++ app/index.js
@@ -1,2 +1,13 @@
-import css from './style.css';
-console.log('Hello World');
+import getHeader from './components/header.js';
+
+import css from './style/app.css';
+
+const app = document.querySelector('#app'),
+      header = getHeader('Gift Exchange');
+
+const render = (container, ...components) => {
+  container.innerHTML = '';
+  components.map((component) => container.insertAdjacentHTML('afterbegin', component));
+}
+
+render(app, header);
