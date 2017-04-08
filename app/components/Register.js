f85ec130740a005da352ae38f12a770e830620d7
Sylvain Hamann <sylvhama@gmail.com>
2017-04-09 00:00:29 +0900

    added disabling button

--- app/components/Register.js
+++ app/components/Register.js
@@ -5,29 +5,56 @@ import css from './register.css';
 const Register = {
   template : `
     <form class="register-form">
-      <input class="input" name="name" type="text" placeholder="Write your name" required />
+      <input class="input main-name" name="name" type="text" placeholder="Write your name" required />
       <label class="label">
         <input class="input input--checkbox spouse-checkbox" type="checkbox" name="spouse" value="yes" /> I came with my spouse.
       </label>
       <div class="spouse-registration spouse-registration--hidden">
         <select class="input spouse-selector">
-          <option selected="selected">Select your spouse</option>
+          <option selected="selected" value="default">Select your spouse</option>
         </select>
         <p class="in-between">Or</p>
-        <input class="input" name="spousename" type="text" placeholder="Add his/her name here"/>
+        <input class="input spouse-name" name="spousename" type="text" placeholder="Register his/her name too"/>
       </div>
       ${Button({caption:'Register'}, {type: 'submit'})}
     </form>
   `,
   controller : (myFirebase) => {
     const registerForm = document.querySelector('.register-form'),
-          spouseCheckbox = document.querySelector('.spouse-checkbox'),
-          spouseRegistration = document.querySelector('.spouse-registration');
+          spouseCheckbox = registerForm.querySelector('.spouse-checkbox'),
+          spouseRegistration = registerForm.querySelector('.spouse-registration'),
+          mainName = registerForm.querySelector('.main-name'),
+          spouseSelector = registerForm.querySelector('.spouse-selector'),
+          spouseName = registerForm.querySelector('.spouse-name'),
+          submit = registerForm.querySelector('button[type=submit]'),
+          today = new Date(),
+          year = today.getFullYear();
     spouseCheckbox.addEventListener('change', function(event){
       spouseRegistration.classList.toggle('spouse-registration--hidden');
     });
     registerForm.addEventListener("submit", function(event) {
       event.preventDefault();
+      const namesToAdd = [];
+      let spouse = '';
+      submit.setAttribute("disabled", "");
+      namesToAdd.push(Promise.resolve(myFirebase.addParticipant(year, mainName.value)));
+      if(spouseSelector.value !== 'default') spouse = spouseSelector.value;
+      if(spouseName.value !== '') spouse = spouseName.value;
+      if(spouse !== '') namesToAdd.push( Promise.resolve(myFirebase.addParticipant(year, spouse)));
+      Promise.all(namesToAdd)
+      .then(() => {
+        mainName.value = '';
+        spouseSelector.value = 'default';
+        spouseName.value = '';
+        spouseCheckbox.checked = false;
+        spouseRegistration.classList.add('spouse-registration--hidden');
+        submit.removeAttribute("disabled");
+      })
+      .catch((error) => {
+        submit.removeAttribute("disabled");
+        console.error(error);
+        alert('An error occured, contact Mr. Smith!')
+      });
     });
   }
 }
