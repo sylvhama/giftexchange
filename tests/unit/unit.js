d26f927d7748577151e1f4da67e2fee2d4ab80e2
Sylvain Hamann <sylvhama@gmail.com>
2017-04-10 13:16:24 +0200

    m

--- tests/unit/unit.js
+++ tests/unit/unit.js
@@ -1,10 +1,36 @@
-import {orderByName} from '../app/common.js';
+import {orderByName, toArrayPeople} from '../../app/common.js';
 
-let ar = [
-  {name:'Xavier'},
-  {name:'Alex'},
-  {name:'Ben'},
-]
+const assert = require('chai').assert;
 
-ar.sort(orderByName);
-console.log(ar);
+describe('orderByName', () => {
+  it('should sort array of people by name', () => {
+    const arr = [
+      {name: 'xavier'},
+      {name: 'Alex'},
+      {name: 'Jon'}
+    ];
+    const expectedRes = [
+      {name: 'Alex'},
+      {name: 'Jon'},
+      {name: 'xavier'}
+    ];
+    arr.sort(orderByName);
+    assert.sameDeepMembers(arr, expectedRes);
+  });
+});
+
+describe('toArrayPeople', () => {
+  it('should create an array from an object', () => {
+    const obj = {
+      'key1': {name: 'xavier', spouse: '', drawn: '', hasBeenDrawn:false},
+      'key2': {name: 'Alex', spouse: '', drawn: '', hasBeenDrawn:false},
+    };
+    const expectedRes = [
+      {key: 'key2', name: 'Alex', spouse: '', drawn: '', hasBeenDrawn:false},
+      {key: 'key1', name: 'xavier', spouse: '', drawn: '', hasBeenDrawn:false}
+    ];
+    const arr = toArrayPeople(obj);
+
+    assert.includeDeepMembers(arr, expectedRes);
+  });
+});
